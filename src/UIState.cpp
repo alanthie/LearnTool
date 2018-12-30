3f776f90f23a6bd68ca2e3e0083f85d1df02d218
Alain Lanthier <alanthier001@hotmail.ca>
2018-12-30 03:58:58 -0500

    add support for mkv files

--- src/UIState.cpp
+++ src/UIState.cpp
@@ -782,7 +782,7 @@ void UIState::render(sf::RenderTarget& renderer)
     if (img_files.size() > 0)
     {
         std::string s = img_files[index_img].extension();
-        if ((s == "mp4") || (s == "avi"))
+        if ((s == "mp4") || (s == "avi") || (s == "mkv"))
         {
             _mode = display_mode::show_movie;
         }
